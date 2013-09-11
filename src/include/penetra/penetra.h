313fb741b64e3faeb2ee37bc45c4318864984570
patito <benatto@gmail.com>
2013-09-11 10:33:44 -0300

    Added docs.

--- src/include/penetra/penetra.h
+++ src/include/penetra/penetra.h
@@ -18,66 +18,102 @@ typedef struct {
 } Penetra;
 
 
-/* 
+/* Initialize PE object (struct Penetra) 
  *
+ * @param pen A valid Penetra object.
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
 extern _u32 penetra_init(Penetra *pen);
 
 
-/* 
+/* Open the file name and load the PE binary to memory. To load
+ * binary file to the memory mmap functions is used by default,
+ * but you can change using penetra_set_alloc_type() or 
+ * penetra_open_malloc(). The libpenetra provides two ways of
+ * load: PENETRA_ALLOC_MMAP and PENETRA_ALLOC_MALLOC.
  *
+ * @param pen A valid Penetra object.
+ * @param fname A valid binary filename. Example: /tmp/putty.exe
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
-extern _u32 penetra_open(Penetra *pen, const char *fp);
+extern _u32 penetra_open(Penetra *pen, const char *fname);
 
 
-/* 
+/* Open and load binary file using mmap() function.
  *
+ * @param pen A valid Penetra object.
+ * @param fname A valid binary filename. Example: /tmp/putty.exe
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
-extern _u32 penetra_open_mmap(Penetra *pen, const char *fp);
+extern _u32 penetra_open_mmap(Penetra *pen, const char *fname);
 
 
-/* 
+/* Open and load binary file using malloc() function.
  *
+ * @param pen A valid Penetra object.
+ * @param fname A valid binary filename. Example: /tmp/putty.exe
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
-extern _u32 penetra_open_malloc(Penetra *pen, const char *fp);
+extern _u32 penetra_open_malloc(Penetra *pen, const char *fname);
 
 
-/* 
+/* Release PE object (struct Penetra) 
  *
+ * @param pen A valid Penetra object.
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
 extern _u32 penetra_finish(Penetra *pen);
 
-/* 
+
+/* Verify if the binary file is a Portable Executable (PE).
  *
+ * @param pen A valid Penetra object.
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
 extern _u32 penetra_is_pe(Penetra *pen);
 
 
+/****************************************************************************** 
+ ********************************** GETTERS *********************************** 
+ *****************************************************************************/ 
 
 
-/*GETTERS */
-
-
-/* 
+/* Get the alloc type.
  *
+ * @param pen A valid Penetra object.
+ * @param alloc_type Saved alloc type.
+ * 
+ *  { ... } 
+ *  _u8 atype;
+ *  Penetra pen;
+ *  penetra_get_alloc_type(&pen, &atype);
+ *  { ... }
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
 extern _u32 penetra_get_alloc_type(Penetra *pen, _u8 *alloc_type);
 
-/* SETTERS */
+
+/****************************************************************************** 
+ ********************************** SETTERS *********************************** 
+ *****************************************************************************/ 
 
 
-/* 
+/* Set the alloc type.
  *
+ * @param pen A valid Penetra object.
+ * @param alloc_type The allocation type to be used.
  *
+ * @return PENETRA_SUCCESS in case of success, otherwise an ERROR.
  */
 extern _u32 penetra_set_alloc_type(Penetra *pen, _u8 alloc_type);
+
 
 PENETRA_END_DECLS
 
